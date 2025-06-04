2959147967bf824eff21cc667ccbc2c07d0b8f35
jimmy <jimmy.balay97@gmail.com>
2025-06-04 15:00:54 +0200

    QUETE4JSMONOREPO

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,21 +1,5 @@
 import express from "express";
 const router = express.Router();
-
-
-
-/* ************************************************************************* */
-
-// Declaration of a "Welcome" route
-
-import sayActions from "./modules/say/sayActions";
-
-router.get("/", sayActions.sayWelcome);
-
-// QUETE 03 - JS MONOREPO : ROUTING
-
-import programActions from "./modules/program/programActions";
-
-router.get("/api/programs", programActions.browse)
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
@@ -28,5 +12,27 @@ router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
+
+/* ************************************************************************* */
+
+// QUETE 03 - JS MONOREPO : ROUTING
+
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
+
+// QUETE 04 - JS MONOREPO : ROUTING AVANCE
+import categoryActions from "./modules/category/categoryActions";
+
+router.get("/api/programs/:id", programActions.read);
+router.get("/api/categories/", categoryActions.browse)
+router.get("/api/categories/:id", categoryActions.read)
+
+
+// Declaration of a "Welcome" route
+
+import sayActions from "./modules/say/sayActions";
+
+router.get("/", sayActions.sayWelcome);
 
 export default router;
